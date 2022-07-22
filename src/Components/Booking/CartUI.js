e27a8826baf99614ad932508f800ca2abd3316f9
navdeep1840 <navikitu001@gmail.com>
2022-07-22 16:47:05 +0530

    cart ui

---
+++ src/Components/Booking/CartUI.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function CartUI() {
+  return (
+    <div>
+      <div class="container m-3">
+        <div class="row">
+          <div class="col-lg-5 col-md-7  p-3 d-flex flex-column ">
+            <h3>ROom Name</h3>
+            <span>MAx Guest </span>
+            <p>Room Desc</p>
+            <span>ROom PRice : price here</span>
+
+            <span>TOtal guest : </span>
+            <span> + 100 for 2 person </span>
+
+            <span> + 350 for extra person </span>
+
+            <span> Total price inc gst : </span>
+
+            <button className="d-flex float-end book-now-btn btn-book ">
+              Book Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CartUI;
