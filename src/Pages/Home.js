2f3cdce1f9c5370ef9762bc40f75912bdf785467
Yogesh Chandra <002chandra.yogesh@gmail.com>
2022-07-22 16:18:44 +0530

    checking

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,10 +10,31 @@ import CardSlider from "../Components/CardSlider/CardSlider";
 import HallAndDiningHome from "../Components/HallAndDiningHome";
 import Amneties from "../Components/Hotel-amneties";
 import RoomSlider from '../Components/RoomSlider/RoomSlider'
-
+import img1 from '../Assets/images/gallery8.jpg'
 import slideimg1 from "../Assets/images/sliderimg-1.jpeg";
 import slideimg2 from "../Assets/images/sliderimg-2.jpeg";
 import slideimg3 from "../Assets/images/sliderimg-3.jpeg";
+
+const array = [
+  {
+    image: img1,
+    title: "title",
+    subTitle: "subtitle",
+    description: "Description"
+  },
+  {
+    image: img1,
+    title: "title",
+    subTitle: "subtitle",
+    description: "Description"
+  },
+  {
+    image: img1,
+    title: "title",
+    subTitle: "subtitle",
+    description: "Description"
+  },
+]
 
 function Home() {
   return (
@@ -22,7 +43,7 @@ function Home() {
       <Slider img1={slideimg1} img2={slideimg2} img3={slideimg3} />
       <AboutUs />
       <RoomSlider />
-      <CardSlider />
+      <CardSlider array={array} />
       <HallAndDiningHome />
       <Amneties />
       <ContactForm />
