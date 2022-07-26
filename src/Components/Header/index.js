c55aeb1fcc275d73d3fcb25e4e13ff1b9fb284c1
Yogesh Chandra <002chandra.yogesh@gmail.com>
2022-07-26 20:48:43 +0530

    styling

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -13,13 +13,16 @@ import logo2 from "../../Assets/images/logo-2.png";
 import { Link } from "react-router-dom";
 import RoomDropdown from "../RoomDropdown";
 import "./index.css";
+import executiveRoom from "../../Assets/images/executive-room.jpeg";
+import superiorRoom from "../../Assets/images/superior-room.jpeg";
+import deluxeRoom from "../../Assets/images/delux-img-4.jpeg";
 
 const Header = () => {
   const [logo, setLogo] = useState(logo1);
   const [width, setWidth] = useState("180");
   const [height, setHeight] = useState("80");
   const [bg, setbg] = useState("");
-  const [showDropdown, setShowDropdown] = useState(false)
+  const [showDropdown, setShowDropdown] = useState(false);
   const changeNavbarLogo = () => {
     if (window.scrollY >= 80) {
       setLogo(logo2);
@@ -76,8 +79,7 @@ const Header = () => {
               <NavDropdown
                 title="rooms"
                 id="navbarScrollingDropdown"
-                renderMenuOnMount={true}
-                // onClick={() =>setShowDropdown(true)}
+                className="d-block d-lg-none"
               >
                 <div className="nav-dropdown">
                   <NavDropdown.Item as={Link} to="/rooms/standard-room">
@@ -98,6 +100,57 @@ const Header = () => {
                 </div>
               </NavDropdown>
 
+              <div>
+                <li class="nav-item dropdown-mega position-static d-none d-lg-block">
+                  <a
+                    class="nav-link dropdown-toggle nav-links mr-2"
+                    href="#"
+                    data-bs-toggle="dropdown"
+                    data-bs-auto-close="outside"
+                  >
+                    Rooms
+                  </a>
+                  <div class="dropdown-menu bg-grey m-0" style={{border: '0px', borderRadius: '0px'}}>
+                    <div class="mega-content px-4">
+                      <div class="container-fluid megamenu">
+                        <div class="row justify-content-between">
+                          <div className="col py-4">
+                            <div>
+                              <img className="img img-fluid" src={executiveRoom} alt="executive room" /> 
+                              <h5 className="text-center dropdown-title my-2">Executive room</h5>
+                            </div>
+                          </div>
+                          <div className="col py-4">
+                            <div>
+                              <img className="img img-fluid" src={superiorRoom} alt="executive room" /> 
+                              <h5 className="text-center dropdown-title my-2">Superior room</h5>
+                            </div>
+                          </div>
+                          <div className="col py-4">
+                            <div>
+                              <img className="img img-fluid" src={deluxeRoom} alt="executive room" /> 
+                              <h5 className="text-center dropdown-title my-2">Deluxe room</h5>
+                            </div>
+                          </div>
+                          <div className="col py-4">
+                            <div>
+                              <img className="img img-fluid" src={executiveRoom} alt="executive room" /> 
+                              <h5 className="text-center dropdown-title my-2">Executive room</h5>
+                            </div>
+                          </div>
+                          <div className="col py-4">
+                            <div>
+                              <img className="img img-fluid" src={executiveRoom} alt="executive room" /> 
+                              <h5 className="text-center dropdown-title my-2">Executive room</h5>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </div>
+
               <Nav.Link
                 as={Link}
                 className="nav-links mr-2"
@@ -107,7 +160,6 @@ const Header = () => {
                 Dining Hall
               </Nav.Link>
 
-
               <Nav.Link
                 as={Link}
                 className="nav-links mr-2"
@@ -116,6 +168,7 @@ const Header = () => {
               >
                 Contact us
               </Nav.Link>
+
               <Nav.Link as={Link} to="/booking">
                 <Button className="book-now-btn">Book Now</Button>
               </Nav.Link>
@@ -123,10 +176,8 @@ const Header = () => {
           </Offcanvas.Body>
         </Navbar.Offcanvas>
       </Container>
-      <RoomDropdown show={showDropdown} />
     </Navbar>
   );
 };
 
 export default Header;
-
