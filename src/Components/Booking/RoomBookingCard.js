56581ba0d7560fd2a93fe1ef179643de1bd972e1
Yogesh Chandra <002chandra.yogesh@gmail.com>
2022-07-29 16:38:13 +0530

    capacity alert

--- src/Components/Booking/RoomBookingCard.js
+++ src/Components/Booking/RoomBookingCard.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useCart } from "react-use-cart";
 import { Link } from "react-router-dom";
 import "./index.css";
+import { Modal, Button } from "react-bootstrap";
 import about2 from "../../Assets/images/about-grid-small.jpg";
 import { useDispatch, useSelector } from "react-redux";
 import { getSpecificROOM } from "../../redux/reducer/Hotel/hotel.action";
@@ -14,9 +15,10 @@ function RoomBookingCard(room) {
   const [belowChild, setBelowChild] = useState("0");
 
   const [price, setPrice] = useState();
+  const [totalGuest, setTotalGuest] = useState();
+  const [showAlert, setShowAlert] = useState(false);
 
   // console.log(parseInt(adult) + parseInt(child) + parseInt(belowChild));
-
 
   const dispatch = useDispatch();
   const [roominfo, setroominfo] = useState({});
@@ -25,48 +27,45 @@ function RoomBookingCard(room) {
     {
       id: 1,
       adult: "1",
-      child_5to7: '0',
-      child_below5: '0',
-    }
+      child_5to7: "0",
+      child_below5: "0",
+    },
   ]);
 
   const reduxState = useSelector((globalState) => globalState.cartReducer.cart);
 
-  const [roomDetails,setRoomDetails]=useState(
-    [
-      {
-      roomname:"",
-      noOfRooms:"",
+  const [roomDetails, setRoomDetails] = useState([
+    {
+      roomname: "",
+      noOfRooms: "",
       noOfAdults: 1,
-    }
-  ]
-  )
+    },
+  ]);
 
   console.log(roomDetails);
   useEffect(() => {
     room._id &&
-      dispatch(getSpecificROOM(room._id)).then((data) => {
-        setroominfo(data.payload);
-        return data.payload;
-      }).then((data) => {
-        console.log(data);
-        reduxState.forEach((each) => {
-          if (each._id === data._id) {
-            setroominfo((prev) => ({ ...prev, isAddedToCart: true }));
-          }
+      dispatch(getSpecificROOM(room._id))
+        .then((data) => {
+          setroominfo(data.payload);
+          return data.payload;
+        })
+        .then((data) => {
+          console.log(data);
+          reduxState.forEach((each) => {
+            if (each._id === data._id) {
+              setroominfo((prev) => ({ ...prev, isAddedToCart: true }));
+            }
+          });
         });
-      });
-;
   }, [reduxState]);
-
 
   const addFoodToCart = () => {
     dispatch(
-      addToCart({roomid:roominfo._id,roomName:roominfo.name, quantity: 1 })
-      );
-      setroominfo((prev) => ({ ...prev, isAddedToCart: true }));
+      addToCart({ roomid: roominfo._id, roomName: roominfo.name, quantity: 1 })
+    );
+    setroominfo((prev) => ({ ...prev, isAddedToCart: true }));
   };
-
 
   // const addRow = () =>{
 
@@ -85,70 +84,78 @@ function RoomBookingCard(room) {
   const handleRoomnoChange = (e) => {
     setRoomNo(e.target.value);
 
-    setRoomDetails({...roomDetails,noOfRooms:e.target.value})
-  }
+    setRoomDetails({ ...roomDetails, noOfRooms: e.target.value });
+  };
 
-    // useEffect(()=>{
-    //   setRoomDetails(current =>{
-    //     current.map(value =>{
-    //       return {...value, noOfAdults: parseInt(adult) + parseInt(child) + parseInt(belowChild)}
-    //     })
-    //   })
-    // }, [adult])
+  // useEffect(()=>{
+  //   setRoomDetails(current =>{
+  //     current.map(value =>{
+  //       return {...value, noOfAdults: parseInt(adult) + parseInt(child) + parseInt(belowChild)}
+  //     })
+  //   })
+  // }, [adult])
 
+  const filtered = selectedRoom.find((value) => {
+    return value.id === 1;
+  });
 
-    const filtered = selectedRoom.find(value => {
-      return value.id === 1
-    });
+  console.log(filtered.adult);
 
-    console.log(filtered.adult);
-
-    const updateAdult = (id, value) => {
-
-        setAdult(value)
+  const updateAdult = (id, value) => {
+    if (
+      parseInt(value) + parseInt(child) + parseInt(belowChild) <=
+      room.guestCapacity
+    ) {
+      setAdult(value);
+    } else {
+      setShowAlert(true);
     }
-    const updateChild_5to7 = (id, value) => {
-
-        setChild(value)
-
+  };
+  const updateChild_5to7 = (id, value) => {
+    if (
+      parseInt(adult) + parseInt(value) + parseInt(belowChild) <=
+      room.guestCapacity
+    ) {
+      setChild(value);
+    } else {
+      setShowAlert(true);
     }
-    const updateBelowChild = (id, value) => {
+  };
+  const updateBelowChild = (id, value) => {
+    if (
+      parseInt(adult) + parseInt(child) + parseInt(value) <=
+      room.guestCapacity
+    ) {
+      setBelowChild(value);
+    } else {
+      setShowAlert(true);
+    }
+  };
 
-        setBelowChild(value)
+  useEffect(() => {
+    // function roomPrice(){
 
+    console.log("inside price");
+    const totalGuest = parseInt(adult) + parseInt(child) + parseInt(belowChild);
+    if (totalGuest == 1) {
+      const totalPrice = room.pricePerNight;
+      setPrice(totalPrice);
+    } else if (totalGuest == 2) {
+      const totalPrice = room.pricePerNight + 100;
+      setPrice(totalPrice);
+    } else {
+      const totalPrice = room.pricePerNight + 100 + (totalGuest - 2) * 300;
+      setPrice(totalPrice);
     }
 
-    useEffect(()=>{
-      // function roomPrice(){
+    setTotalGuest(totalGuest);
 
-        console.log("inside price")
-        const totalGuest = parseInt(adult) + parseInt(child) + parseInt(belowChild);
-        if(totalGuest == 1)
-        {
-          const totalPrice = room.pricePerNight;
-          setPrice(totalPrice)
-        }
-        else if(totalGuest == 2)
-        {
-          const totalPrice = room.pricePerNight + 100;
-          setPrice(totalPrice)
-        }
-        else{
-          const totalPrice = room.pricePerNight + 100 + (totalGuest - 2)*300;
-          setPrice(totalPrice)
-        }
+    // }
+  }, [adult, child, belowChild]);
 
-        
-      // }
-  
+  console.log(selectedRoom);
+  console.log(parseInt(roomNo));
 
-    }, [adult, child, belowChild])
-
-
-    console.log(selectedRoom)
-    console.log(parseInt(roomNo));
-
-    
   return (
     <>
       <div className="container m-3">
@@ -200,66 +207,96 @@ function RoomBookingCard(room) {
             )}
             </button> */}
 
-            {(roomNo !== '0' && <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col"></th>
-                  <th scope="col">Adult <br /></th>
-                  <th scope="col">Child <br /> <small className="text-muted">(Age 5-12 yrs)</small> </th>
-                  <th scope="col">Child <br /> <small className="text-muted">(Below 5yrs)</small> </th>
-                  <th scope="col">Room Price <br /> <small className="text-muted">for 1 night(s)</small> </th>
-                </tr>
-              </thead>
-              <tbody>
-                  {
-                    [...Array(parseInt(roomNo))].map((room, key) => {
-                      return (<tr>
-                      
-                      <th scope="row"></th>
-                      <td>
-                        <select
-                          className="form-select form-select-sm"
-                          aria-label=".form-select-sm example"
-                          value={adult}
-                          onChange={(e)=> updateAdult(0, e.target.value)}
-                        >
-                          <option value="1">1 Adult</option>
-                          <option value="2">2 Adult</option>
-                          <option value="3">3 Adult</option>
-                        </select>
-                      </td>
-                      <td>
-                        <select
-                          className="form-select form-select-sm"
-                          aria-label=".form-select-sm example"
-                          value={child}
-                          onChange={(e)=>updateChild_5to7(0, e.target.value)}
-                        >
-                          <option value="0">0 Child</option>
-                          <option value="1">1 Child</option>
-                        </select>
-                      </td>
-                      <td>
-                        <select
-                          className="form-select form-select-sm"
-                          aria-label=".form-select-sm example"
-                          value={belowChild}
-                          onChange={(e)=>updateBelowChild(0, e.target.value)}
-                        >
-                          <option value="0">0 Child</option>
-                          <option value="1">1 Child</option>
-                        </select>
-                      </td>
-                      <td>
-                        <span>INR {price}</span>
-                      </td>
-                      <button
-                      className="btn add-cart-btn"
-                        onClick={addFoodToCart}
-                        disabled={roominfo?.isAddedToCart}> Add to Cart
-                      </button>
+            <Modal show={showAlert} onHide={() => setShowAlert(false)}>
+              <Modal.Header closeButton>
+                <Modal.Title>{room.name}</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                This room can only accomodate only {room.guestCapacity} persons
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={() => setShowAlert(false)}>
+                  OK
+                </Button>
+              </Modal.Footer>
+            </Modal>
 
-
+            {roomNo !== "0" && (
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col"></th>
+                    <th scope="col">
+                      Adult <br />
+                    </th>
+                    <th scope="col">
+                      Child <br />{" "}
+                      <small className="text-muted">(Age 5-12 yrs)</small>{" "}
+                    </th>
+                    <th scope="col">
+                      Child <br />{" "}
+                      <small className="text-muted">(Below 5yrs)</small>{" "}
+                    </th>
+                    <th scope="col">
+                      Room Price <br />{" "}
+                      <small className="text-muted">for 1 night(s)</small>{" "}
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {[...Array(parseInt(roomNo))].map((room, key) => {
+                    return (
+                      <tr>
+                        <th scope="row"></th>
+                        <td>
+                          <select
+                            className="form-select form-select-sm"
+                            aria-label=".form-select-sm example"
+                            value={adult}
+                            onChange={(e) => updateAdult(0, e.target.value)}
+                          >
+                            <option value="1">1 Adult</option>
+                            <option value="2">2 Adult</option>
+                            <option value="3">3 Adult</option>
+                          </select>
+                        </td>
+                        <td>
+                          <select
+                            className="form-select form-select-sm"
+                            aria-label=".form-select-sm example"
+                            value={child}
+                            onChange={(e) =>
+                              updateChild_5to7(0, e.target.value)
+                            }
+                          >
+                            <option value="0">0 Child</option>
+                            <option value="1">1 Child</option>
+                          </select>
+                        </td>
+                        <td>
+                          <select
+                            className="form-select form-select-sm"
+                            aria-label=".form-select-sm example"
+                            value={belowChild}
+                            onChange={(e) =>
+                              updateBelowChild(0, e.target.value)
+                            }
+                          >
+                            <option value="0">0 Child</option>
+                            <option value="1">1 Child</option>
+                          </select>
+                        </td>
+                        <td>
+                          <span>INR {price}</span>
+                        </td>
+                        <button
+                          className="btn add-cart-btn"
+                          onClick={addFoodToCart}
+                          disabled={roominfo?.isAddedToCart}
+                        >
+                          {" "}
+                          Add to Cart
+                        </button>
                       </tr>
                     );
                   })}
