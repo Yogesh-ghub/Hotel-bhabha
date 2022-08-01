c35054c6c56d5485b640f4ce47e006f2a5b7fab0
navdeep1840 <navikitu001@gmail.com>
2022-08-01 23:05:29 +0530

    backend deployed

--- src/redux/reducer/booking/Booking.action.js
+++ src/redux/reducer/booking/Booking.action.js
@@ -1,37 +1,43 @@
-import { POST_BOOKING_DETAILS, POST_USER_DATA, POST_CART_DATA } from "./Booking.type";
+import {
+  POST_BOOKING_DETAILS,
+  POST_USER_DATA,
+  POST_CART_DATA,
+} from "./Booking.type";
 import axios from "axios";
 
 export const postDetails = (details) => async (dispatch) => {
+  try {
+    const datarecieve = await axios.post(
+      "https://hotel-bhabha-backend.herokuapp.com/api/booking",
+      details
+    );
 
-    try {
-
-        const datarecieve = await axios.post('http://localhost:4000/api/booking', details)
-
-
-        return dispatch({ type: POST_BOOKING_DETAILS, payload: datarecieve.data })
-    }
-    catch (error) {
-        return dispatch({ type: "ERROR", payload: error })
-    }
-}
+    return dispatch({ type: POST_BOOKING_DETAILS, payload: datarecieve.data });
+  } catch (error) {
+    return dispatch({ type: "ERROR", payload: error });
+  }
+};
 export const postCartDetails = (details) => async (dispatch) => {
-    try {
-        const datarecieve = await axios.post('http://localhost:4000/api/booking/cart', details)
+  try {
+    const datarecieve = await axios.post(
+      "https://hotel-bhabha-backend.herokuapp.com/api/booking/cart",
+      details
+    );
 
-        return dispatch({ type: POST_CART_DATA, payload: datarecieve.data })
-    }
-    catch (error) {
-        return dispatch({ type: "ERROR", payload: error })
-    }
-}
+    return dispatch({ type: POST_CART_DATA, payload: datarecieve.data });
+  } catch (error) {
+    return dispatch({ type: "ERROR", payload: error });
+  }
+};
 export const postUserDetails = (details) => async (dispatch) => {
-    try {
-        const datarecieve = await axios.post('http://localhost:4000/api/booking/guest', details)
+  try {
+    const datarecieve = await axios.post(
+      "https://hotel-bhabha-backend.herokuapp.com/api/booking/guest",
+      details
+    );
 
-
-        return dispatch({ type: POST_USER_DATA, payload: datarecieve.data })
-    }
-    catch (error) {
-        return dispatch({ type: "ERROR", payload: error.message })
-    }
-}
+    return dispatch({ type: POST_USER_DATA, payload: datarecieve.data });
+  } catch (error) {
+    return dispatch({ type: "ERROR", payload: error.message });
+  }
+};
