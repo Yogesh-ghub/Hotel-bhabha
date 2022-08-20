a4bc056ba981d6b2e534b0ae0f6e5eccd4bc862a
Yogesh Chandra <002chandra.yogesh@gmail.com>
2022-08-20 18:23:20 +0530

    about-col-change

--- src/Components/AboutUs/index.js
+++ src/Components/AboutUs/index.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import img1 from "../../Assets/images/DSC00754.jpg";
-import food2 from '../../Assets/images/food2.jpg'
+import food2 from "../../Assets/images/food2.jpg";
 
 import { Container, Row, Col, Button } from "react-bootstrap";
 
-import { IoBed, IoRestaurant } from 'react-icons/io5'
-
+import { IoBed, IoRestaurant } from "react-icons/io5";
 
 import "./index.css";
 const AboutUs = () => {
-
   let navigate = useNavigate();
 
   function handleClick(path) {
@@ -21,7 +19,7 @@ const AboutUs = () => {
     <Container className="p-4 about-us-container">
       <Row className="d-flex justify-content-between">
         <Col md={12} lg={6}>
-          <Row>
+          {/* <Row>
             <Col
               md={6}
               className="col-area my-4"
@@ -77,6 +75,48 @@ const AboutUs = () => {
                 </Col>
               </Row>
             </Col>
+          </Row> */}
+          <Row>
+            <Col xs={12} sm={6} className="col-area my-4">
+              <div className="text-area">
+                <div className="d-flex justify-content-center my-2">
+                  <IoBed size={48} className="" />
+                </div>
+                <h4 className="text-center my-2">Cozy Bed</h4>
+                <p className="text-center">
+                  Far Far away, behind the word mountains, far from countries
+                  vokalia
+                </p>
+              </div>
+            </Col>
+            <Col xs={12} sm={6} className="col-area my-4">
+              <div className="about-us-img">
+                <figure>
+                  <img src={img1} alt="About us " width="100%" height="100%" />
+                </figure>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} sm={6} className="col-area my-4">
+              <div className="about-us-img">
+                <figure>
+                  <img src={food2} alt="About us" width="100%" height="100%" />
+                </figure>
+              </div>
+            </Col>
+            <Col xs={12} sm={6} className="col-area my-4">
+              <div className="text-area">
+                <div className="d-flex justify-content-center my-2">
+                  <IoRestaurant size={48} className="" />
+                </div>
+
+                <h4 className="text-center my-2">Best Food</h4>
+                <p className="text-center">
+                  A Premier Gujrati Thali with Delicious Varieties
+                </p>
+              </div>
+            </Col>
           </Row>
         </Col>
 
@@ -92,7 +132,10 @@ const AboutUs = () => {
               to the pride of Rajkot Maintaining the manner and comfort of times
               past...
             </p>
-            <Button className="book-now-btn" onClick={() => handleClick('./about')}>
+            <Button
+              className="book-now-btn"
+              onClick={() => handleClick("./about")}
+            >
               Read more...
             </Button>
           </div>
